e7cf2d71cec9e8e6a19b93b2d2488e47c3284fcb
Ayodele <awoleyeayodele1@gmail.com>
2016-12-23 18:59:24 -0500

    Added Model Repositories

---
+++ src/CarRentalApplication.Repositories/ReservationRepository.cs
@@ -0,0 +1,69 @@
+﻿using CarRentalApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalApplication.Repositories
+{
+    public class ReservationRepository
+    {
+        private AppDbContext _context;
+        private UserManager<AppUser> _userManager;
+
+        public ReservationRepository(AppDbContext context, UserManager<AppUser> userMgr)
+        {
+            _context = context;
+            _userManager = userMgr;
+        }
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return _context.Reservations.ToList();
+        }
+
+        public Reservation FindReservationByConfirmationNumber(long confirmationNumber)
+        {
+            return _context.Reservations.Find(confirmationNumber);
+        }
+
+        public bool AddNewReservation(Reservation newReservation, string userId)
+        {
+            var user = _context.Users.Find(userId);
+            if (user != null)
+            {
+                user.Reservations.Add(newReservation);
+                _context.SaveChanges();
+
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateReservation(Reservation reservationToUpdate)
+        {
+            _context.Reservations.Attach(reservationToUpdate);
+            _context.Entry(reservationToUpdate).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return true;
+        }
+
+        public Reservation FindReservationByUser(long confirmationNumber, AppUser user)
+        {
+            return _context.Reservations.Where(x => x.ConfirmationNumber == confirmationNumber && string.Compare(x.AppUserId, user.Id) == 0).FirstOrDefault();
+        }
+
+        public bool DeleteReservation(Reservation reservationToDelete)
+        {
+            _context.Reservations.Remove(reservationToDelete);
+            var isdeleted = _context.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<Reservation> GetReservationsForUser(string userId)
+        {
+            return _context.Reservations.Where(x => x.AppUserId == userId).ToList();
+        }
+    }
+}
---
+++ src/CarRentalApplication.Repositories/VehicleRepository.cs
@@ -0,0 +1,48 @@
+﻿using CarRentalApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalApplication.Repositories
+{
+    public class VehicleRepository
+    {
+        private AppDbContext _context;
+
+        public VehicleRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Vehicle> GetAll()
+        {
+            return _context.Vehicles.ToList();
+        }
+
+        public IEnumerable<Vehicle> GetAllAvailableVehicles()
+        {
+            return _context.Vehicles.Where(x => x.IsAvailable).ToList();
+        }
+
+        public Vehicle GetVehicleById(Guid id)
+        {
+            return _context.Vehicles.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Vehicle> GetVehicleByFilter(VehicleType vt)
+        {
+            if (vt == VehicleType.All)
+            {
+                return GetAll();
+            }
+            return _context.Vehicles.Where(x => x.ModelType == vt);
+        }
+
+        public void AddNewVehicle(Vehicle newVehicle)
+        {
+            _context.Vehicles.Add(newVehicle);
+            _context.SaveChanges();
+        }
+    }
+}
