b099ab165f295113df272003660c664392bce376
Ayodele <awoleyeayodele1@gmail.com>
2017-01-10 23:28:29 -0500

    Add Reservation Review Form

--- src/CarRentalApplication.Models/ViewModels/Reservation/ReservationViewModel.cs
+++ src/CarRentalApplication.Models/ViewModels/Reservation/ReservationViewModel.cs
@@ -13,6 +13,22 @@ namespace CarRentalApplication.Models.ViewModels.Reservation
 
         public ReservationContactViewModel ContactSetup { get; set; }
 
+        public int TotalRentalDays
+        {
+            get { return Convert.ToInt32((this.LogisticsSetup.ReturnDate - this.LogisticsSetup.PickupDate).TotalDays); }
+        }
+
+        public double TotalVehicleCostMinusTax
+        {
+            get
+            {
+                if (this.TotalRentalDays > 0)
+                {
+                    return (this.VehicleSetup.Vehicle.PricePerDay * TotalRentalDays);
+                }
+                return this.VehicleSetup.Vehicle.PricePerDay;
+            }
+        }
         public double StateTax
         {
             get { return 24.49; }
@@ -21,6 +37,11 @@ namespace CarRentalApplication.Models.ViewModels.Reservation
         public double FederalTax
         {
             get { return 14.29; }
+        }
+
+        public double TotalRentalCost
+        {
+            get { return this.TotalVehicleCostMinusTax + FederalTax + StateTax; }
         }
 
         public FormSubmissionViewModel FormProcessing { get; set; }
